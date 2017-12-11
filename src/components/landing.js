9bb1b58da75e351d2ede559b0785900e7a17a470
Micaela Linda Villani <villani.micaela@gmail.com>
2017-12-11 19:33:19 +0100

    finish burger menu

--- src/components/landing.js
+++ src/components/landing.js
@@ -14,14 +14,13 @@ class Landing extends React.Component {
           <img src="../images/mic-icon.png" className="my-icon" alt="personal-icon"/>
         </div>
         <div className="description-container">
-        <h2 className="hello"> hello! </h2>
         <p className="introduction"> 
-          i'm 
-          <span className="name">Micaela</span>
+          Hello, i'm 
+          <span className="name">Micaela</span>!
         </p>
         <p className="description"> 
           <i className="em em-8ball"></i>
-          Front-end Developer
+          <span className="first-row-description"> Front-end Developer </span>
           <i className="em em-female-astronaut"></i>
           <br/>
           Animation Lover  
@@ -34,6 +33,9 @@ class Landing extends React.Component {
         </p>
         </div>
         <div className="social-links-container">
+          <a href="mailto:villani.micaela@gmail.com?Subject=Hello%20again" className="social-logo">
+              <img src="../images/mail.png" alt="e-mail"/> 
+            </a>
           <a href="https://www.linkedin.com/in/micaelavillani/" className="social-logo">
             <img src="../images/linkedin.png" alt="linkedin"/> 
           </a>
@@ -50,31 +52,3 @@ class Landing extends React.Component {
 }
 
 export default Landing;
-
-/* <h2>
-      <img src="./images/mic-icon.png" class="my-icon" alt="personal-icon"/>
-    </h2>
-    <p> 
-      <span class="presentation"> hello, I'm 
-        <span class="name">Micaela</span>,
-      </span><br/>
-      <span class="first-row">
-        <i class="em em-8ball"></i>
-        Front-end Developer<i class="em em-female-astronaut"></i>
-      </span><br/>
-      Animation Lover <i class="em em-sparkling_heart"></i>| 
-      Learner <i class="em em-book"></i>| 
-      Music Addict <i class="em em-headphones"></i></br>
-      learn more <i class="em em-point_down"></i>| check my socials             
-    </p>
-    <div class="social-links-container">
-      <a href="https://www.linkedin.com/in/micaelavillani/" target="_blank" class="social-logo">
-        <img src="./images/linkedin.png" alt="linkedin">
-      </a>
-      <a href="https://github.com/MicaelaLV" target="_blank" class="social-logo">
-        <img src="./images/github.png"  alt="linkedin">
-      </a>
-      <a href="https://www.instagram.com/thexperimentaldev/" target="_blank" class="social-logo">
-        <img src="./images/instagram.png"  alt="linkedin">
-      </a>
-    </div> */
--- src/components/menu.js
+++ src/components/menu.js
@@ -9,12 +9,16 @@ class AnimatedMenu extends React.Component {
 
   render() {
     return (
-      <Menu width={ '14%' } isOpen={ false }>
+      <Menu width={ '14%' } isOpen={ false } customBurgerIcon={ <img src="../images/burger.png" alt="burger-icon"/> } >
+        <a className="menu-item mic" href="/"> 
+          <img src="../images/mic-icon.png" alt="mic logo" className="mic"/>
+        </a>
         <a id="home" className="menu-item" href="/">Hello</a>
         <a id="skills" className="menu-item" href="/skills">Skills</a>
         <a id="portfolio" className="menu-item" href="/portfolio">Portfolio</a>
         <a id="about" className="menu-item" href="/about">About me</a>
-        <a id="contact" className="menu-item" href="/contact">Get in Touch</a>
+        <a id="contact" className="menu-item" href="/contact">Contacts</a>
+        <a id="copywrite" className="copy" href="/"> @ MLV 2017</a>
       </Menu>
     )
   }
